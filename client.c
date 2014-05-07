7f5bb11a62375528f35e8a88b570df86bf95767e
Jason Malutich <malutj@users.noreply.github.com>
2014-05-07 17:03:47 -0500

    Create client.c

---
+++ client.c
@@ -0,0 +1,15 @@
+/* * * * * * * * * * * * * * * * * * * * * *
+*   Filename: client.c
+*   Owner: Jason M Malutich
+*   Date: 7 May 2014
+* * * * * * * * * * * * * * * * * * * * * */
+
+#include "client.h"
+
+
+
+
+int main(int argc, char **argv){
+
+    return 0;
+}
