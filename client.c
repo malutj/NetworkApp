2d71b66d983661272c9c40eefd69f5f8ac615648
Jason Malutich <malutj@users.noreply.github.com>
2014-05-07 17:21:45 -0500

    Update client.c

--- client.c
+++ client.c
@@ -7,9 +7,39 @@
 #include "client.h"
 
 
-
-
 int main(int argc, char **argv){
-
+    
+    //variables
+    char *host;
+    char input[BUF_SIZE];
+    int port;
+    int controlCon;
+    
+    
+    
+    //get command line arguments
+    if(argc != 3) printUsage();
+    host = argv[1];
+    port = atoi(argv[2]);
+    
+    //create socket for control connection
+    //connect to host on specified port number
+    //enter loop
+    do{
+        
+    }while(getInput() != "exit");
+    
+    
+    
     return 0;
 }
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * 
+ * Desc: Prints usage instructions for program
+ * Return: void
+ * * * * * * * * * * * * * * * * * * * * * * * */
+ void printUsage(){
+     puts("Usage: client <hostname> <port number>\n");
+     exit(0);
+ }
