51cd24cbb3888dde3ea890fd3d07b927acf87dfd
Jason Malutich <malutj@users.noreply.github.com>
2014-05-07 17:05:05 -0500

    Create server.h

---
+++ server.h
@@ -0,0 +1,12 @@
+/* * * * * * * * * * * * * * * * * * * * * *
+*   Filename: server.h
+*   Owner: Jason M Malutich
+*   Date: 7 May 2014
+* * * * * * * * * * * * * * * * * * * * * */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/type.h>
+#include <sys/socket.h>
+#include <netdb.h>
