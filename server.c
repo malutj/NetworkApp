8b2c87e6b565c35ae05e551f20c6f488f3f29749
Jason Malutich <malutj@users.noreply.github.com>
2014-05-07 17:05:53 -0500

    Create server.c

---
+++ server.c
@@ -0,0 +1,13 @@
+/* * * * * * * * * * * * * * * * * * * * * *
+*   Filename: server.c
+*   Owner: Jason M Malutich
+*   Date: 7 May 2014
+* * * * * * * * * * * * * * * * * * * * * */
+
+#include "server.h"
+
+
+int main(int argc, char **argv){
+
+    return 0;
+}
