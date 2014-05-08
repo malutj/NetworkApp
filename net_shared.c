11fc659a546dd83392be8cec30fa5947ec7ab438
Jason Malutich <malutj@users.noreply.github.com>
2014-05-08 17:04:08 -0500

    Update net_shared.c

--- net_shared.c
+++ net_shared.c
@@ -1,4 +1,13 @@
+/* * * * * * * * * * * * * * * * * * * * * *
+ *  Filename: net_shared.c
+ *  Owner: Jason M Malutich
+ *  Date: 7 May 2014
+ *  Desc: Contains functions shared by both
+ *        the client and server programs
+ * * * * * * * * * * * * * * * * * * * * * */
+ 
 #include "net_shared.h"
+
 
 /* * * * * * * * * * * * * * * * * * * * * * * * 
  *  Desc: Prints error messages
@@ -8,4 +17,17 @@
 void print_error(char *msg){
     fprintf(stderr, "ERROR: %s\n", msg);
     exit(EXIT_FAILURE);
+}
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ *  Desc: Create a new socket
+ *  Return: int - the file descriptor for the new socket
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+int get_socket(void){
+    int fd;
+    if((fd = socket(PF_INET, SOCK_STREAM, 0) == -1){
+        print_error(strerror(errno));
+    }
+    return fd;
 }
