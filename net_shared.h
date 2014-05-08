41b72641a101bd365f05ce8094d9d1737d107ad9
Jason Malutich <malutj@users.noreply.github.com>
2014-05-08 17:04:36 -0500

    Update net_shared.h

--- net_shared.h
+++ net_shared.h
@@ -1,10 +1,26 @@
+/* * * * * * * * * * * * * * * * * * * * * *
+ *  Filename: net_shared.h
+ *  Owner: Jason M Malutich
+ *  Date: 7 May 2014
+ *  Desc: Contains functions shared by both
+ *        the client and server programs
+ * * * * * * * * * * * * * * * * * * * * * */
+
 #include <stdio.h>
 
 #define DEBUG(MSG) DEBUG_ON ? printf("%s",MSG) : "";
 
-//define commands
-
+//define statements
+//commands
 #define EXIT 0
 #define LIST 1
 #define GET 2
 #define CD 3
+
+//buffer sizes
+#define BUF_SIZE 1000
+#define MSG_SIZE_BUF 4
+
+//shared function declarations
+void    print_error     (char *msg);
+int     get_socket      (void);
