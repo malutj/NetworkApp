3da98797167c3f72353f50cf46c820e616e54ed9
Jason Malutich <malutj@users.noreply.github.com>
2014-05-08 17:05:05 -0500

    Update and rename server.h to net_server.h

--- net_server.h
+++ net_server.h
@@ -1,5 +1,5 @@
 /* * * * * * * * * * * * * * * * * * * * * *
-*   Filename: server.h
+*   Filename: net_server.h
 *   Owner: Jason M Malutich
 *   Date: 7 May 2014
 * * * * * * * * * * * * * * * * * * * * * */
@@ -10,3 +10,12 @@
 #include <sys/type.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <pthread.h>
+#include "net_shared.h"
+
+#define DEBUG_ON 1
+
+//function declarations
+void    print_usage     (void);
+void    bind_socket     (int socket, char *port);
+void    *session_handler (void *);
